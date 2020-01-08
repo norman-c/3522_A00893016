9c9dc775a3fb7451e94b5831aa4fd8c65a2b21a6
norman-c <52259325+norman-c@users.noreply.github.com>
2020-01-07 16:29:01 -0800

    Hypotenuse and Calculator

--- Labs/Lab 0/calculator.py
+++ Labs/Lab 0/calculator.py
@@ -1,4 +1,3 @@
-
 def sum(a, b):
     return a + b
 
@@ -6,28 +5,33 @@ def sum(a, b):
 def subtract(a, b):
     return a - b
 
+
 def multiply(a, b):
     return a * b
+
 
 def divide(a, b):
     return a / b
 
-def selector(a,b,c):
+
+def selector(a, b, c):
     if c == 1:
-        return sum(a,b)
+        return sum(a, b)
     elif c == 2:
-        return subtract(a,b)
+        return subtract(a, b)
     elif c == 3:
-        return multiply(a , b)
+        return multiply(a, b)
     elif c == 4:
         return divide(a, b)
+
 
 def main():
     a = int(input("Enter first number: "))
     b = int(input("Enter second number: "))
     print("1 to add\n2 to subtract\n3 to multiply\n4 to divide")
     c = int(input())
-    print(selector(a,b,c))
+    print(selector(a, b, c))
+
 
 if __name__ == '__main__':
     main()
