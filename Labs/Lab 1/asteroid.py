3c58ab0350a30be98fa073de099b263e8c0935f8
norman-c <52259325+norman-c@users.noreply.github.com>
2020-01-14 17:09:55 -0800

    simulation created

--- Labs/Lab 1/asteroid.py
+++ Labs/Lab 1/asteroid.py
@@ -5,20 +5,20 @@ class Asteroid:
 
     unique_id = 0
 
-    def __init__(self, circumference, position, velocity, id):
+    def __init__(self, circumference, position, velocity):
         self._circumference = circumference
         self._position = position
         self._velocity = velocity
-        self._id = id
+        self._id = Asteroid.get_unique_id()
 
     @classmethod
     def get_unique_id(cls):
         cls.unique_id +=1
         return cls.unique_id
 
-    @classmethod
-    def calc_circumference(cls, radius):
-        return (radius * 2 * math.pi)
+    @staticmethod
+    def calc_circumference(radius):
+        return radius * 2 * math.pi
 
     def __str__(self):
         return "Id: {0} Circumference: {1} Position: {2} Velocity: {3}".format(self._id, self._circumference, self._position, self._velocity)
@@ -26,19 +26,19 @@ class Asteroid:
     def set_circumference(self, circumference):
         self._circumference = circumference
 
-    def get_circumference(self, circumference):
+    def get_circumference(self):
         return self._circumference
 
-    def set_position(self, position):
-        self._position = position
+    def set_position(self, new_position):
+        self._position = new_position
 
-    def get_position(self, position):
+    def get_position(self):
         return self._position
 
     def set_velocity(self, velocity):
         self._velocity = velocity
 
-    def get_velocity(self, velocity):
+    def get_velocity(self):
         return self._velocity
 
     def move(self):
--- Labs/Lab 1/controller.py
+++ Labs/Lab 1/controller.py
@@ -1,12 +1,35 @@
 import random
-import math
-import asteroid as asteroid
+import time
+import datetime
+from asteroid import Asteroid
+
 
 
 class Controller:
+
     asteroid_list = []
 
     def __init__(self):
-        for i in range(1, 100):
+        for i in range(1, 101):
             circumference = Asteroid.calc_circumference(random.randint(1,4))
-            temp_asteroid = asteroid(circumference, 0, (random.randint(1, 5), random.randint(1, 5), random.randint(1, 5)))
+            temp_asteroid = Asteroid(circumference, (0,0,0), (random.randint(1, 5), random.randint(1, 5), random.randint(1, 5)))
+            Controller.asteroid_list.append(temp_asteroid)
+
+    def simulate(self, seconds):
+        print("Simultation Start Time: {}".format(datetime.datetime.fromtimestamp(time.time())))
+        print("\nMoving Asteroids!\n-----------------\n")
+        for i in range(seconds):
+            time.sleep(1)
+            for x in Controller.asteroid_list:
+                x.set_position(x.move())
+                print(x)
+            print("\n")
+        print("Simultation End Time: {}".format(datetime.datetime.fromtimestamp(time.time())))
+
+
+def main():
+    temp = Controller()
+    temp.simulate(2)
+
+if __name__ == "__main__":
+    main()
